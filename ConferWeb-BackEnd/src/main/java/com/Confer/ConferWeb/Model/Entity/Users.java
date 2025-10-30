8aa38d076b9f3ef188f16692d44fb456b3fbbe0b
Gabriel Menezes <gabrielmenezesb2@outlook.com>
2025-10-30 12:55:49 -0300

    Rename database table from 'user' to 'users'

--- ConferWeb-BackEnd/src/main/java/com/Confer/ConferWeb/Model/Entity/Users.java
+++ ConferWeb-BackEnd/src/main/java/com/Confer/ConferWeb/Model/Entity/Users.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
